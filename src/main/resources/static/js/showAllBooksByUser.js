49ab69e14c6a3c4ecdfa98d38b5293dfd6a6d60c
Marek.Michels <marekmichels@gmx.net>
2019-06-10 19:12:24 +0200

    Added ratings

--- src/main/resources/static/js/showAllBooksByUser.js
+++ src/main/resources/static/js/showAllBooksByUser.js
@@ -5,6 +5,7 @@ $.get('/books/by/user/current', function (books) {
         '<th>Cover</th>' +
         '<th>Title</th>' +
         '<th>Author</th>' +
+        '<th>You´re Rating</th>' +
         '</tr>' +
         '</thead>' +
         '<tbody>';
@@ -16,6 +17,7 @@ $.get('/books/by/user/current', function (books) {
         tr += '<td><img src="' + books[i].coverReference + '" alt="coverImage" style="width: 50%; height: 50%"></td>';
         tr += '<td>' + books[i].title + '</td>';
         tr += '<td>' + books[i].author + '</td>';
+        tr += '<td>' + getRatingAsStars(books[i].rating) + '</td>';
         tr += '</tr>';
         html += tr;
     }
@@ -23,3 +25,11 @@ $.get('/books/by/user/current', function (books) {
 
     $('#myBooks-table').html(html);
 });
+
+function getRatingAsStars(rating) {
+    let ratingHTML = '';
+    for (let i = 0; i < rating; i++) {
+        ratingHTML += '<img src="/img/star_full.png" alt="star_full" style="height: 32px; width: 32px">';
+    }
+    return ratingHTML;
+}
